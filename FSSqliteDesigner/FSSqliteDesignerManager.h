19934981142e869ff8424579e24e3d901bdd7fd3
fengsh <fengsh@fsh-mac--00-24-D6-72-E4-6A.local>
2016-08-23 15:38:09 +0800

    上传代码。

--- FSSqliteDesigner/FSSqliteDesignerManager.h
+++ FSSqliteDesigner/FSSqliteDesignerManager.h
@@ -5,6 +5,10 @@
 //  Created by fengsh on 7/7/15.
 //  Copyright © 2015年 fengsh. All rights reserved.
 //
+/*
+    插件安装路径
+        ~/Library/Application Support/Developer/Shared/Xcode/Plug-ins/
+ */
 
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
